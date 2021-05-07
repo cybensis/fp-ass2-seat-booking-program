2def4af74eeff6d99cd92fb8e5ba351f83cb70b0
Guy Seccull s3785085 <s3785085@student.rmit.edu.au>
2021-05-07 14:07:25 +1000

    Starting on user panel

---
+++ src/main/controller/user/CreateBookingController.java
@@ -0,0 +1,50 @@
+package main.controller.user;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CreateBookingController {
+
+    @FXML
+    private Label isConnected;
+
+    @FXML
+    private Text newBooking;
+
+    @FXML
+    private Text manageBooking;
+
+    @FXML
+    private ImageView backButton;
+
+    @FXML
+    public void goBack(MouseEvent event) throws IOException {
+        Parent loginView = FXMLLoader.load(getClass().getClassLoader().getResource("main/ui/login.fxml"));
+        Stage window = (Stage) backButton.getScene().getWindow();
+        window.setScene(new Scene(loginView));
+        window.show();
+    }
+
+    @FXML
+    void manageBookingNav(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newBookingNav(MouseEvent event) throws IOException {
+        Parent loginView = FXMLLoader.load(getClass().getClassLoader().getResource("main/ui/user/createBooking.fxml"));
+        Stage window = (Stage) backButton.getScene().getWindow();
+        window.setScene(new Scene(loginView));
+        window.show();
+    }
+
+}
